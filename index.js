54874f24a338954df55ef53d2ecae7d6bf5e3aaf
Chloe Wright <cwright25@wustl.edu>
2018-01-10 12:33:44 -0600

    fixed links (?)

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-    
 var links = document.links;
-for (var link in links) {
-    link.target = '_blank';
-}
     
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+    links[i].target = '_blank';
+}
 });
+
+//used code from https://gist.github.com/CrocoDillon/7989214
